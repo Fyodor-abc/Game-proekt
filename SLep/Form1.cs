41c03488cf2bfd317e2c71351a74a38bf6695ec1
Федя Трошин <fyodor.troshin@gmail.com>
2021-12-09 23:10:02 +0300

    a

--- SLep/Form1.cs
+++ SLep/Form1.cs
@@ -13,9 +13,14 @@ namespace SLep
     public partial class Form1 : Form
     {
         int click = 0;
-        Point templocation = new Point(0, 0);
+        Point cellocation = new Point(0, 0);
+        Point Hantlocation;
+        Point p;
+        Point d;
+        private bool n = false;
         Random rnd = new Random();
-        bool mouse = false;
+        bool poz = false;
+        
         public Form1()
         {
             InitializeComponent();
@@ -24,28 +29,55 @@ namespace SLep
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+
+             if (d.X >= cellocation.X && d.Y >= cellocation.Y)
+             {
+                 click++;
+                 label1.Text = Convert.ToString(click);
+                 poz = true;
+                 if (poz)
+                 {
+                     pictureBox1.Location = cellocation;
+                     cellocation.X = rnd.Next(panel1.Width - 50);
+                     pictureBox1.Location = cellocation;
+                     cellocation.Y = rnd.Next(panel1.Height - 50);
+                 }
+             
         }
 
- 
+    }
 
-       private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
         {
-            click++;
-            label1.Text = Convert.ToString(click);
-            mouse = true;
-            if (mouse)
+        }
+        private void pictureBox2_MouseDown(object sender, MouseEventArgs e)
+        {
+            Hantlocation = PointToClient(MousePosition);
+            p = new Point(Hantlocation.X - pictureBox2.Location.X, Hantlocation.Y - pictureBox2.Location.Y);
+            n = true;
+        }
+
+        private void pictureBox2_MouseUp(object sender, MouseEventArgs e)
+        {
+            n = false;
+        }
+
+        private void pictureBox2_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (n)
             {
-                pictureBox1.Location = templocation;
-                templocation.X = rnd.Next(panel1.Width - 50);
-                pictureBox1.Location = templocation;
-                templocation.Y = rnd.Next(panel1.Height - 50);
+                Hantlocation = PointToClient(MousePosition);
+                d = new Point(Hantlocation.X - p.X, Hantlocation.Y - p.Y);
+                pictureBox2.Location = d;
             }
-       }
+        }
 
-        private void pictureBox1_Click(object sender, EventArgs e)
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
         {
-
+            Point a = new Point(e.X, e.Y);
+            pictureBox2.Location = a;
         }
     }
 }
